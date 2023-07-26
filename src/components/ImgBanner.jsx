e491cd160d8d1b33f99c665c91933a66d0dac820
Nadau07 <noah.liebert@outlook.fr>
2023-07-26 15:20:47 +0200

    Commencement responsive design

--- src/components/ImgBanner.jsx
+++ src/components/ImgBanner.jsx
@@ -4,7 +4,7 @@ function ImgBanner(){
     return (
         <div className='divImgBanner'>
             <img className='imgBanner' src='Image_source_1.jpg' alt='imgBanner'/>
-            <h1 className='titleImgBanner'>Chez vous, partout et ailleurs</h1>
+            <h1 className='titleImgBanner'><span className='spanBanner'>Chez vous,</span> partout et ailleurs</h1>
         </div>
     )
 }
