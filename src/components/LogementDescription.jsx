e36583beef9878f2cc2a54906a06394880f7af2c
Nadau07 <noah.liebert@outlook.fr>
2023-08-11 12:50:53 +0200

    Page "A propos" terminée

--- src/components/LogementDescription.jsx
+++ src/components/LogementDescription.jsx
@@ -1,16 +1,26 @@
+import { useState } from 'react'
 import '../styles/LogementDescription.css'
 
 
 function FicheLogementComponents4(props){
 
+const [ParagrapheVisible , setParagrapheVisible ] = useState(false)
+
+const ParagrapheActif = () =>{
+    setParagrapheVisible(!ParagrapheVisible);
+}
+
+
     return(
 
         <div className='component4Totalite'>
             <p className='logementDescription'>
-                <span>{props.title}</span><span><i className="fa-solid fa-chevron-down"></i></span></p>
-            <p className='paragrapheDescription'>{props.content}</p>
+                <span>{props.title}</span><span><i className="fa-solid fa-chevron-down" onClick= {ParagrapheActif}></i></span></p>
+            {ParagrapheVisible && <p className='paragrapheDescription'>{props.content}</p>} 
         </div>
-
+        
+        /* si ma condition Paragrapheisible est false alors le paragraphe ne s'affiche pas
+        Quand on clique sur l'icone, la fonction "ParagrapheActif s'execute :la fonction prendra le contraire de l'etat actuel du paragraphe (c-a-d non visible car false) et l'affichera */
     )
 }
 
--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,10 +1,11 @@
 import Footer from '../components/Footer'
 import '../styles/Apropos.css'
 import {NavLink} from 'react-router-dom'
+import LogementDescription from '../components/LogementDescription'
 
 function Apropos(){
     return(
-        <div>
+        <div className='AproposGeneral'>
             <div className='banner'>
         <NavLink to={`/`}>
         <img className='banner_logo'src='logo.jpg' alt='logo'/>
@@ -13,7 +14,19 @@ function Apropos(){
         <NavLink to={`/Apropos`}>A propos</NavLink>
     </div>
             <img className='imgBanner2' src='Image_source_2.jpg' alt='ImgBanner2' />
-            <div>EMPLACEMENT DES RUBRIQUES </div>
+            <div className='Apropos-container'>
+                <LogementDescription title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+ régulièrement vérifiées par nos équipes." />
+                <LogementDescription title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+                <LogementDescription title="Service" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+perturbation du voisinage entraînera une exclusion de notre plateforme." />
+                <LogementDescription title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
+également des ateliers sur la sécurité domestique pour nos hôtes." />
+
+            </div>
             <Footer />
         </div>
     )
