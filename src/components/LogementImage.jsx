af1495ecd7c36325507a97344c39aed4433354b0
Nadau07 <noah.liebert@outlook.fr>
2023-08-11 15:55:32 +0200

    Affichage des chevrons et numerotation OK

--- src/components/LogementImage.jsx
+++ src/components/LogementImage.jsx
@@ -22,22 +22,32 @@ function LogementImage(props) {
     ImgActuelle((Img -1 ) % pictures.length -1)
 }
 
-    return (
-        
-    
-
-        <div className='ImageLogement' >
-           {pictures.map((picture, i)=>(<img  key={picture} src={picture} alt="pictures" className={ClassActive(i)}
-             ></img>))}
-                <button onClick={ImagePrecedente} className='BoutonPrecedent'><i class="fa-solid fa-chevron-left"></i></button>
-          
-        <button onClick={ImageSuivante} className='BoutonSuivant'><i class="fa-solid fa-chevron-right"></i></button>
-        </div>
-        
-        
-        
-    );
+return (
+    <div className='ImageLogement'>
+        {pictures.map((picture, i) => (
+            <img key={picture} src={picture} alt="pictures" className={ClassActive(i)}></img>
+        ))}
+        {pictures.length > 1 && (
+            <div>
+                <button onClick={ImagePrecedente} className='BoutonPrecedent'>
+                    <i className="fa-solid fa-chevron-left"></i>
+                </button>
+                <button onClick={ImageSuivante} className='BoutonSuivant'>
+                    <i className="fa-solid fa-chevron-right"></i>
+                </button>
+            </div>
+        )}
+     
+            {/* Numérotation des images */}
+            {pictures.length > 1 && (
+                <div className='Numerotation'>
+                    {Img + 1}/{pictures.length}
+                </div>
+            )}
+    </div>
+);
 }
+/*  {pictures.lenght >1 && ... : affiche les boutons et la numérotation uniquement si la longueur du tableau est supérieure à une image } */
 
 
 
