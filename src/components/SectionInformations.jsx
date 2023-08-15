3b6756379034efd588a687fe455db5ee9d33d7d3
Nadau07 <noah.liebert@outlook.fr>
2023-08-15 11:18:11 +0200

    Suppression du border-radius + chevron  up/down (sectionInformations)

--- src/components/SectionInformations.jsx
+++ src/components/SectionInformations.jsx
@@ -17,19 +17,24 @@ function SectionInformations(props) {
 
   return (
     <div className="descriptionTotalite">
-      <p className="sectionInformation">
-        <span>{props.title}</span>
-        <span>
+    <p className="sectionInformation">
+      <span>{props.title}</span>
+      <span>
+        {ParagrapheVisible ? (
+          <i className="fa-solid fa-chevron-up" onClick={ParagrapheActif}></i>
+        ) : (
           <i className="fa-solid fa-chevron-down" onClick={ParagrapheActif}></i>
-        </span>
-      </p>
-      {ParagrapheVisible && (
-        <p className="sectionParagraphe">{props.content}</p>
-      )}
-    </div>
+        )}
+      </span>
+    </p>
+    {ParagrapheVisible && (
+      <p className="sectionParagraphe">{props.content}</p>
+    )}
+  </div>
 
     /* si ma condition Paragraphevisible est "false" alors le paragraphe ne s'affiche pas.
-        Quand on clique sur l'icone, la fonction "ParagrapheActif s'execute :la fonction prendra le contraire de l'etat actuel du paragraphe (c-a-d non visible car false) et l'affichera */
+        Quand on clique sur l'icone, la fonction "ParagrapheActif s'execute :la fonction prendra le contraire de l'etat actuel du paragraphe (c-a-d non visible car false) et l'affichera 
+      */
   );
 }
 
